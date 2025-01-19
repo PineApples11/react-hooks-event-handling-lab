25ef7b6a96a0bd0c7aa8d27d73f836f0d4392760
PineApples11 <kinyanjuiivy1@gmail.com>
2025-01-19 09:22:40 +0300

    event listeners practise

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+    function handleChange(event){
+        
+        // console.log('${event.target.name}: ${event.target.value}');
+        console.log("Entering password...")
+    }
+  return (
+    <div>
+      <input type="password" name="password" placeholder="password" onChange={handleChange} />
+
+    </div>
+  );
 }
 
 export default Keypad;
